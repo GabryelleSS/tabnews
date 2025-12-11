d7b32e7b5fe84138c4c96838c0a041cf70d240c0
GabryelleSS <gabryelle-gaby@hotmail.com>
2025-12-11 15:01:44 -0300

    fix: .env.development envs

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: process.env.POSTGRESS_USER,
-    password: process.env.POSTGRESS_PASSWORD,
-    host: process.env.POSTGRESS_HOST,
-    port: process.env.POSTGRESS_PORT,
-    database: process.env.POSTGRESS_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
   const result = client.query(queryObject);
